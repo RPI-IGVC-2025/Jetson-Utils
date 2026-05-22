d7600b63e362e461aec658a2a4b27d388507f343
TagCiccone <tagciccone@gmail.com>
2026-05-22 18:57:32 -0400

    Working compilation all the way to ros:jazzy

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -1,14 +1,11 @@
-while getopts "v:" flag; do
-    case "${flag}" in
-        v) cd ros-jazzy-core
-           docker build --build-arg L4T_VER=${OPTARG} -t ros2-l4t:jazzy-core
-           echo Building ros-jazzy-core complete!
-           cd ../ros-jazzy
-           docker build -t ros2-l4t:jazzy
-           cd ..
-           echo Building ros-jazzy complete!
-           ;;
-    esac
-done
+# This is a build script instead of one long dockerfile because I like having roughly the same inheritance pipeline
+# as the normal images.
+
+docker build -t l4t-24:r36.2.0 l4t-base-image/
+echo Building l4t complete!
+docker build -t ros2-l4t:jazzy-core ros-jazzy-core/
+echo Building ros-jazzy-core complete!
+docker build -t ros2-l4t:jazzy ros-jazzy
+echo Building ros-jazzy complete!
 
 echo Image build complete!
