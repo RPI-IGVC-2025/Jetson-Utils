672b1990e71386534c066b7d26103504376a1aa2
TagCiccone <tagciccone@gmail.com>
2026-05-26 14:54:55 -0400

    add ands to build script

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -1,11 +1,10 @@
 # This is a build script instead of one long dockerfile because I like having roughly the same inheritance pipeline
 # as the normal images.
 
-docker build -t l4t-24:r36.2.0 l4t-base-image/
-echo Building l4t complete!
-docker build -t ros2-l4t:jazzy-core ros-jazzy-core/
-echo Building ros-jazzy-core complete!
-docker build -t ros2-l4t:jazzy ros-jazzy
-echo Building ros-jazzy complete!
-
+docker build -t l4t-24:r36.2.0 l4t-base-image/ &&
+echo Building l4t complete! &&
+docker build -t ros2-l4t:jazzy-core ros-jazzy-core/ &&
+echo Building ros-jazzy-core complete! &&
+docker build -t ros2-l4t:jazzy ros-jazzy &&
+echo Building ros-jazzy complete! &&
 echo Image build complete!
