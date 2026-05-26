2f1df17324c216c2a609dc047e6358cd012cfb80
TagCiccone <tagciccone@gmail.com>
2026-05-26 18:39:03 -0400

    change regex

--- docker/mount_opencv.sh
+++ docker/mount_opencv.sh
@@ -1,1 +1,1 @@
-ls /usr/lib | grep libopencv | sudo bash -c "sed 's/^/lib, /' > /etc/nvidia-container-runtime/host-files-for-container.d/cv.csv"
+ls /usr/lib | grep libopencv_.*\.so\.4\.8\.0 | sudo bash -c " sed 's/^/lib, /' > /etc/nvidia-container-runtime/host-files-for-container.d/cv.csv"
